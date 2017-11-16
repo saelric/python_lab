05456f8c5d35064990a4d7973f7c73d9da964c55
saelric <saelric@gmail.com>
2017-11-15 23:25:25 -0600

    Refs #7 arreglo de complejidad

--- python_lab_2.py
+++ python_lab_2.py
@@ -105,9 +105,7 @@ def is_leap_year(year):
     >>> is_leap_year(2016)
     True
     """
-    if year % 400 == 0:
-        print(True)
-    elif year % 4 == 0 and year % 100 != 0:
+    if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
         print(True)
     else:
         print(False)
