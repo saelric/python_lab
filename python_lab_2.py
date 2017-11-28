ad66f152fb08d513b8e178a361b2d83518ff3613
saelric <saelric@gmail.com>
2017-11-27 23:06:08 -0600

    strong Typing sin problemas

--- python_lab_2.py
+++ python_lab_2.py
@@ -1,13 +1,13 @@
-from typing import Tuple
 from typing import List
-from typing import Dict
-from typing import Any
-
+from typing import Tuple
+from math import sqrt
+from contracts import contract
 # -*- coding: utf-8 -*-
 """ Repaso interactivo de python
 """
 
 
+@contract(lower='int,>0', upper='int,>0', returns=None)
 def lower_up(lower: int, upper: int) -> None:
     """ 1: Returns a list of numbers from the lower number to the upper number:
     >>> lower_up(5,15)
@@ -27,64 +27,80 @@ def lower_up(lower: int, upper: int) -> None:
         print(x)
 
 
-def all_the_args(*args: Tuple[int], **kwargs: Dict[str, int]) -> None:
-
-    """ 2: Return an array. Use * to expand positional
-    args and use ** to expand keyword args
-
+@contract(args='tuple[N],N>0', kwargs='dict(str: (int))', returns=None)
+def all_the_args(*args: str, **kwargs: str) -> None:
+    """ 2: Return an array. Use * to expand positional args
+    and use ** to expand keyword args
     >>> all_the_args(1, 2, a=3, b=4)
     (1, 2)
-    [['a', 3], ['b', 4]]
+    {'a': 3, 'b': 4}
     """
-    lists = []
     print(args)
-    for key, value in sorted(kwargs.items()):
-        temp = [key, value]
-        lists.append(temp)
-    print(lists)
+    print(kwargs)
 
 
-def may_20(*tup: int) -> None:
+@contract(tup='tuple[N],N>0', returns=None)
+def may_20(tup: Tuple[int]) -> None:
     """ 3: Definir una tupla con 10 números.
     Imprimir la cantidad de números superiores a 20.
-
-    >>> may_20(10, 16, 22, 26, 27, 30)
+    >>> may_20((10, 16, 22, 26, 27, 30))
     22, 26, 27, 30
-
     """
-    lists = []
-
+    superiores = ""
     for x in tup:
-        lists.append(may_20_2(x))
-    y = str(list(filter(None, lists)))
-    print(y[1:-1])
+        superiores = compara_20(superiores, x)
+
+    print(superiores)
 
 
-def may_20_2(x: int) -> Any:
-    if x > 20:
-        return x
+@contract(cad_temp='str', cadena='int,>0', returns='str')
+def compara_20(cad_temp: str, cadena: int) -> str:
+    """ Comparamos si es mayor a 20
+    """
+    if cadena > 20:
+        cad_temp = concatena(cad_temp, cadena)
+    return cad_temp
+
+
+@contract(cad_temp='str', cadena='int,>0', returns='str')
+def concatena(cad_temp: str, cadena: int) -> str:
+    """ Concatenamos una cadena si esta o no vacia.
+    """
+    if cad_temp == "":
+        cad_temp = str(cadena)
     else:
-        return None
+        cad_temp = cad_temp + ", " + str(cadena)
+    return cad_temp
 
 
-def word_filter(list_of_words: List[str], n: int) -> None:
+@contract(list_='list(str)', n='int,>0', returns='list(str)')
+def word_filter(list_: List[str], n: int) -> List[str]:
     """ 4: Filtra las palabras que contienen más de n caracteres.
-    >>> word_filter(['hello','bye', 'computer', 'software', 'python'], 5)
+    >>> word_filter(['hello', 'bye', 'computer', 'software', 'python'], 5)
     ['computer', 'software', 'python']
     """
-    lists = []
-
-    for item in list_of_words:
-        word_filter_2(item, lists, n)
-
-    print(lists)
-
-
-def word_filter_2(item: str, lists: List[str], n: int) -> None:
-    if len(item) > n:
-        lists.append(item)
+    a = 1
+    for i in range(len(list_)):
+        list_ = word_list(list_, n, a, i)
+        a = word_accepted(list_, n, a, i)
+    return list_
 
 
+@contract(li='list(str)', n='int,>0', a='int,>0', i='int', returns='list(str)')
+def word_list(li: List[str], n: int, a: int, i: int) -> List[str]:
+    if len(li[i-a]) <= n:
+        li.remove(li[i-a])
+    return li
+
+
+@contract(list_='list(str)', n='int,>0', a='int,>0', i='int', returns='int')
+def word_accepted(list_: List[str], n: int, a: int, i: int) -> int:
+    if len(list_[i-a]) <= n:
+        a = a + 1
+    return a
+
+
+@contract(list='str', returns='None')
 def string_length(list: List[str]) -> None:
     """ 5: imprime el largo de una cadena de caracteres
 
@@ -94,6 +110,7 @@ def string_length(list: List[str]) -> None:
     print(len(list))
 
 
+@contract(x='str', returns='None')
 def is_vocal(x: str) -> None:
     """ 6: Determines if it is vocal
 
@@ -108,6 +125,7 @@ def is_vocal(x: str) -> None:
         print(False)
 
 
+@contract(year='int', returns=None)
 def is_leap_year(year: int)-> None:
     """ 7: Determines if a year is a leap year.
 
@@ -120,6 +138,7 @@ def is_leap_year(year: int)-> None:
         print(False)
 
 
+@contract(word='str', returns='int')
 def has_uppercase(word: str) -> int:
     """ 8: Evaluate if a word has uppercase letters
 
@@ -129,6 +148,7 @@ def has_uppercase(word: str) -> int:
     return sum(1 for i in word if i.isupper())
 
 
+@contract(cadena='str', returns='int')
 def contar_vocales(cadena: str) -> int:
     """ 9: Return number of vocales in a word.
 
@@ -149,17 +169,22 @@ def contar_vocales_2(x: str)-> int:
         return 0
 
 
-def square(lists: List[int]) -> None:
+@contract(list='list(int)', returns=None)
+def square(list: List[int]) -> None:
     """ 10: Calculate the square of the numbers in a list
     >>> l = [0, 1, 2, 3]
     >>> square(l)
     [0, 1, 4, 9]
     """
-    print(list(map(lambda x: x**2, lists)))
+    temp = []
+    for i in range(len(list)):
+        temp.append(list[i]*list[i])
+
+    print(temp)
 
 
+@contract(n='int', returns='bool')
 def is_prime(n: int)-> bool:
-    import math
     """ 11:  Return if n is prime.
 
     >>> is_prime(5)
@@ -169,18 +194,17 @@ def is_prime(n: int)-> bool:
     """
     if n % 2 == 0 and n > 2:
         return False
-    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+    return all(n % i for i in range(3, int(sqrt(n)) + 1, 2))
 
 
+@contract(n='int', returns='int')
 def factorial(n: int) -> int:
-    import math
     """ 12: Return the factorial of n, an exact integer >= 0.
     If the result is small enough to fit in an int, return an int.
     Else return a long.
-
     >>> [factorial(n) for n in range(6)]
     [1, 1, 2, 6, 24, 120]
-    >>> [factorial(math.long(n)) for n in range(6)]
+    >>> [factorial(int(n)) for n in range(6)]
     [1, 1, 2, 6, 24, 120]
     >>> factorial(30)
     265252859812191058636308480000000
@@ -192,6 +216,7 @@ def factorial(n: int) -> int:
         return n * factorial(n-1)
 
 
+@contract(n='int', returns='str')
 def to_roman(n: int)-> str:
     """ 13: Convert number integer to Roman numeral
 
@@ -211,7 +236,8 @@ def to_roman(n: int)-> str:
     return str(list).replace("'", "")
 
 
-def rima(word1: str, word2: str) -> str:
+@contract(word1='str', word2='str', returns=None)
+def rima(word1: str, word2: str) -> None:
     """ 14: Indica si dos palabrar riman. Si coinciden las 3 ultimas letras rima,
     si ncoinciden solo 2 rima un poco, si coincide solo 1 no rima.
 
@@ -225,7 +251,7 @@ def rima(word1: str, word2: str) -> str:
     if word1[len(word1)-1] == word2[len(word2)-1]:
         rima3(word1, word2)
     else:
-        return 'no rima'
+        print('no rima')
 
 
 def rima3(word1: str, word2: str) -> None:
@@ -242,7 +268,8 @@ def rima2(word1: str, word2: str) -> None:
         print('rima un poco')
 
 
-def capital(pesos: int, interes: int, anios: int) -> None:
+@contract(pesos='int', interes='float', anios='int', returns=None)
+def capital(pesos: int, interes: float, anios: int) -> None:
     """ 15: Pide una cantidad de pesos, una tasa de interés y un numero de años.
     Muestra en cuanto se habrá convertido el capital inicial transcurridos esos
     años si cada año se aplica la tasa de interés introducida.
